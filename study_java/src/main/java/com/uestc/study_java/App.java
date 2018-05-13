92c0d08018969f536df446e3ca92e9456670b053
ouhuihao <862162990@qq.com>
2018-05-13 15:11:57 +0800

    添加Properties加载
    Signed-off-by: ouhuihao <862162990@qq.com>

---
+++ study_java/src/main/java/com/uestc/study_java/App.java
@@ -0,0 +1,23 @@
+package com.uestc.study_java;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+   
